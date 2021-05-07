3f1ed10dcdaa29888eeea4224d24f450a8095677
Alan Islas <alan.islas.itic@gmail.com>
2021-05-07 09:23:28 -0500

    Component created

---
+++ secret-santa.js
@@ -0,0 +1,9 @@
+import { LitElement, html, css } from 'lit-element';
+
+class SecretSanta extends LitElement {
+  render() {
+    return html`Prueba`;
+  }
+}
+
+customElements.define('secret-santa', SecretSanta);
